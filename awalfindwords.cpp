fe335251421af6e0abbb2b5563cb89b31d53c052
Sabrina0908 <151342946+Sabrina0908@users.noreply.github.com>
2024-04-27 22:25:13 +0700

    awalfindwords.cpp

---
+++ awalfindwords.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+bool searcWord (vector<string>&matrix, string word){
+int rows = matrix.size ();
+int cols = matrix[0].size ();
